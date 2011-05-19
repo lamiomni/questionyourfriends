be0bbd6fe782aa8d05628b610e59dc55921ebfb0
enixdu94@gmail.com <enixdu94@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-05-19 22:26:01 +0000

    - DataGen - DB update - AutoPoco - BackEnd --

---
+++ trunk/QuestionsYourFriends/QuestionsYourFriendsDataGen/DataSources/RandomSqlRowDataSource.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Objects;
+using System.Linq;
+using AutoPoco.Engine;
+
+namespace QuestionsYourFriendsDataGen.DataSources
+{
+    public class RandomSqlRowDataSource<T> : DatasourceBase<T> where T : class
+    {
+        private readonly ObjectSet<T> _set;
+        private readonly Func<T, bool> _where;
+
+        public RandomSqlRowDataSource(ObjectSet<T> set) : this(set, null) { }
+
+        public RandomSqlRowDataSource(ObjectSet<T> set, Func<T, bool> where)
+        {
+            _set = set;
+            _where = where;
+        }
+
+        public override T Next(IGenerationContext context)
+        {
+            if (_where != null)
+                return _set.AsParallel().Where(_where).Random();
+            return _set.Random();
+        }
+    }
+
+}
