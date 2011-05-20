9411020aacadd6a8fc69da8d4b4644d97dff6642
lechevalierd3on@gmail.com <lechevalierd3on@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-05-20 10:45:16 +0000

    Controllers and views for the 3 main pages

---
+++ trunk/QuestionYourFriends/QuestionYourFriends/Controllers/MyQuestionsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuestionYourFriends.Controllers
+{
+    public class MyQuestionsController : Controller
+    {
+        //
+        // GET: /MyQuestions/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
