612422905f0f4b6ec26482f5062193bf9e82c8f6
lechevalierd3on <lechevalierd3on@gmail.com>
2011-05-20 10:45:16 +0000

    Controllers and views for the 3 main pages

---
+++ QuestionYourFriends/QuestionYourFriends/Controllers/AskController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuestionYourFriends.Controllers
+{
+    public class AskController : Controller
+    {
+        //
+        // GET: /Ask/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
