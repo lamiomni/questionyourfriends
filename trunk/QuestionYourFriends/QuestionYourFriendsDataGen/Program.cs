13adf95ef0b09a99df19e720c048506d325fb5d2
enixdu94@gmail.com <enixdu94@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-05-20 13:12:25 +0000

    dB update

--- trunk/QuestionYourFriends/QuestionYourFriendsDataGen/Program.cs
+++ trunk/QuestionYourFriends/QuestionYourFriendsDataGen/Program.cs
@@ -26,17 +26,20 @@ namespace QuestionYourFriendsDataGen
                 x.Include<Question>()
                     .Setup(q => q.anom_price).Use<RandomIntegerSource>(250)
                     .Setup(q => q.date_pub).Use<DateOfBirthSource>()
-                    .Setup(q => q.Owner).Use<RandomSqlRowDataSource<User>>(_qyfe.Users)
-                    .Setup(q => q.Receiver).Use<RandomSqlRowDataSource<User>>(_qyfe.Users)
+                    .Setup(q => q.Owner).Use<RandomSqlDataSource<User>>(_qyfe.Users)
+                    .Setup(q => q.Receiver).Use<RandomSqlDataSource<User>>(_qyfe.Users)
                     .Setup(q => q.private_price).Use<RandomIntegerSource>(250)
-                    .Setup(q => q.undesirable).Use<RandomBooleanSource>();
+                    .Setup(q => q.undesirable).Use<RandomBooleanSource>()
+                    .Setup(q => q.deleted).Use<RandomBooleanSource>();
              
                 x.AddFromAssemblyContainingType<Transac>();
                 x.Include<Transac>()
                     .Setup(t => t.fid).Use<RandomLongSource>()
                     .Setup(t => t.amount).Use<RandomIntegerSource>(25000)
-                    .Setup(t => t.User).Use<RandomSqlRowDataSource<User>>(_qyfe.Users)
-                    .Setup(t => t.status).Use<RandomStringSource>(6, 8);
+                    .Setup(t => t.User).Use<RandomSqlDataSource<User>>(_qyfe.Users)
+                    .Setup(t => t.status).Use<RandomEnumSource<TransacStatus>>()
+                    .Setup(t => t.type).Use<RandomEnumSource<TransacType>>()
+                    .Setup(t => t.Question).Use<RandomSqlDataSource<Question>>(_qyfe.Questions);
 
                 x.AddFromAssemblyContainingType<User>();
                 x.Include<User>()
@@ -62,53 +65,54 @@ namespace QuestionYourFriendsDataGen
 
         public static void CleanDb()
         {
-            Console.WriteLine(@"- Questions...");
-            var questions = _qyfe.Questions;
-            foreach (var question in questions)
-                _qyfe.Questions.DeleteObject(question);
-
-            Console.WriteLine(@"- Transactions...");
+            Console.Write(@"- Transactions.");
             var transactions = _qyfe.Transacs;
             foreach (var transaction in transactions)
                 _qyfe.Transacs.DeleteObject(transaction);
+            Console.WriteLine(@"..");
 
-            Console.WriteLine(@"- Users...");
+            Console.Write(@"- Questions.");
+            var questions = _qyfe.Questions;
+            foreach (var question in questions)
+                _qyfe.Questions.DeleteObject(question);
+            Console.WriteLine(@"..");
+
+            Console.Write(@"- Users.");
             var users = _qyfe.Users;
             foreach (var user in users)
                 _qyfe.Users.DeleteObject(user);
+            Console.Write(@".");
 
             _qyfe.SaveChanges();
+            Console.WriteLine(@".");
         }
 
         public static void AddData()
         {
-            Console.WriteLine(@"- Users...");
-            var users = _session.List<User>(100).Get();
+            const int nbUser = 100;
+            const int nbTransac = 300;
+            const int nbQuestion = 200;
+
+            Console.Write(@"- Users.");
+            var users = _session.List<User>(nbUser).Get();
             foreach (var user in users)
             {
                 //Debug.WriteLine(string.Format("{0} - {1} - {2} - {3}", user.id, user.credit_amount, user.activated, user.fid));
                 _qyfe.Users.AddObject(user);
             }
+            Console.Write(@".");
             _qyfe.SaveChanges();
+            Console.WriteLine(@".");
 
-            Console.WriteLine(@"- Transacs...");
-            var transacs = _session.List<Transac>(100).Get();
-            foreach (var transac in transacs)
-            {
-                //Debug.WriteLine(string.Format("{0} - {1} - {2} - {3} - {4}", transac.id, transac.fid, transac.amount, transac.status, transac.userId));
-                _qyfe.Transacs.AddObject(transac);
-            }
-            _qyfe.SaveChanges();
-
-            Console.WriteLine(@"- Questions...");
+            Console.Write(@"- Questions.");
             var lis = new LoremIpsumSource();
             var rnd = new Random(1337);
-            var questions = _session.List<Question>(100)
-                .First(50)
+            var questions = _session.List<Question>(nbQuestion)
+                .First(nbQuestion / 2)
                     .Impose(q => q.date_answer, DateTime.Now)
                     .Impose(q => q.answer, lis.Next(null).Substring(0, rnd.Next(240, 480)) + ".")
                     .Impose(q => q.text, lis.Next(null).Substring(0, rnd.Next(120, 240)) + "?")
-                .Next(50)
+                .Next(nbQuestion / 2)
                     .Impose(q => q.text, lis.Next(null).Substring(0, rnd.Next(120, 240)) + "?")
                 .All()
                     .Get();
@@ -118,7 +122,20 @@ namespace QuestionYourFriendsDataGen
                 //    question.id_owner, question.id_receiver, question.private_price, question.text, question.undesirable)); 
                 _qyfe.Questions.AddObject(question);
             }
+            Console.Write(@".");
             _qyfe.SaveChanges();
+            Console.WriteLine(@".");
+
+            Console.Write(@"- Transacs.");
+            var transacs = _session.List<Transac>(nbTransac).Get();
+            foreach (var transac in transacs)
+            {
+                //Debug.WriteLine(string.Format("{0} - {1} - {2} - {3} - {4}", transac.id, transac.fid, transac.amount, transac.status, transac.userId));
+                _qyfe.Transacs.AddObject(transac);
+            }
+            Console.Write(@".");
+            _qyfe.SaveChanges();
+            Console.WriteLine(@".");
         }
     }
 }
