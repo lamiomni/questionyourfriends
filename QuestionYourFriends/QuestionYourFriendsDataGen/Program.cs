1ed7200a3803bbfc135658d51f7bfa05001fe599
Tony Nguyen <lamiomni@users.noreply.github.com>
2011-05-20 10:18:45 +0000

    dataGen

--- QuestionYourFriends/QuestionYourFriendsDataGen/Program.cs
+++ QuestionYourFriends/QuestionYourFriendsDataGen/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Diagnostics;
+//using System.Diagnostics;
 using AutoPoco;
 using AutoPoco.DataSources;
 using AutoPoco.Engine;
@@ -29,7 +29,6 @@ namespace QuestionYourFriendsDataGen
                     .Setup(q => q.Owner).Use<RandomSqlRowDataSource<User>>(_qyfe.Users)
                     .Setup(q => q.Receiver).Use<RandomSqlRowDataSource<User>>(_qyfe.Users)
                     .Setup(q => q.private_price).Use<RandomIntegerSource>(250)
-                    .Setup(q => q.text).Use<RandomStringSource>(120, 240)
                     .Setup(q => q.undesirable).Use<RandomBooleanSource>();
              
                 x.AddFromAssemblyContainingType<Transac>();
@@ -63,17 +62,17 @@ namespace QuestionYourFriendsDataGen
 
         public static void CleanDb()
         {
-            Console.WriteLine(@"Cleaning questions...");
+            Console.WriteLine(@"- Questions...");
             var questions = _qyfe.Questions;
             foreach (var question in questions)
                 _qyfe.Questions.DeleteObject(question);
 
-            Console.WriteLine(@"Cleaning transactions...");
+            Console.WriteLine(@"- Transactions...");
             var transactions = _qyfe.Transacs;
             foreach (var transaction in transactions)
                 _qyfe.Transacs.DeleteObject(transaction);
 
-            Console.WriteLine(@"Cleaning users...");
+            Console.WriteLine(@"- Users...");
             var users = _qyfe.Users;
             foreach (var user in users)
                 _qyfe.Users.DeleteObject(user);
@@ -83,30 +82,40 @@ namespace QuestionYourFriendsDataGen
 
         public static void AddData()
         {
-            Console.WriteLine(@"Adding users...");
+            Console.WriteLine(@"- Users...");
             var users = _session.List<User>(100).Get();
             foreach (var user in users)
             {
-                Debug.WriteLine(string.Format("{0} - {1} - {2} - {3}", user.id, user.credit_amount, user.activated, user.fid));
+                //Debug.WriteLine(string.Format("{0} - {1} - {2} - {3}", user.id, user.credit_amount, user.activated, user.fid));
                 _qyfe.Users.AddObject(user);
             }
             _qyfe.SaveChanges();
 
-            Console.WriteLine(@"Adding transacs...");
+            Console.WriteLine(@"- Transacs...");
             var transacs = _session.List<Transac>(100).Get();
             foreach (var transac in transacs)
             {
-                Debug.WriteLine(string.Format("{0} - {1} - {2} - {3} - {4}", transac.id, transac.fid, transac.amount, transac.status, transac.userId));
+                //Debug.WriteLine(string.Format("{0} - {1} - {2} - {3} - {4}", transac.id, transac.fid, transac.amount, transac.status, transac.userId));
                 _qyfe.Transacs.AddObject(transac);
             }
             _qyfe.SaveChanges();
 
-            Console.WriteLine(@"Adding questions...");
-            var questions = _session.List<Question>(100).Get();
+            Console.WriteLine(@"- Questions...");
+            var lis = new LoremIpsumSource();
+            var rnd = new Random(1337);
+            var questions = _session.List<Question>(100)
+                .First(50)
+                    .Impose(q => q.date_answer, DateTime.Now)
+                    .Impose(q => q.answer, lis.Next(null).Substring(0, rnd.Next(240, 480)) + ".")
+                    .Impose(q => q.text, lis.Next(null).Substring(0, rnd.Next(120, 240)) + "?")
+                .Next(50)
+                    .Impose(q => q.text, lis.Next(null).Substring(0, rnd.Next(120, 240)) + "?")
+                .All()
+                    .Get();
             foreach (var question in questions)
             {
-                Debug.WriteLine(string.Format("{0} - {1} - {2} - {3} - {4} - {5} - {6} - {7} - {8} - {9}", question.id, question.anom_price, question.answer, question.date_answer, question.date_pub,
-                    question.id_owner, question.id_receiver, question.private_price, question.text, question.undesirable)); 
+                //Debug.WriteLine(string.Format("{0} - {1} - {2} - {3} - {4} - {5} - {6} - {7} - {8} - {9}", question.id, question.anom_price, question.answer, question.date_answer, question.date_pub,
+                //    question.id_owner, question.id_receiver, question.private_price, question.text, question.undesirable)); 
                 _qyfe.Questions.AddObject(question);
             }
             _qyfe.SaveChanges();
