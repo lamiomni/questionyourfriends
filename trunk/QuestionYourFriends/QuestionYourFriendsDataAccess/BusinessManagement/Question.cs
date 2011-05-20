5b2e515920b17a998f21a0367864c107f666deae
enixdu94@gmail.com <enixdu94@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-05-20 15:49:32 +0000

    da, bm & dataGen

--- trunk/QuestionYourFriends/QuestionYourFriendsDataAccess/BusinessManagement/Question.cs
+++ trunk/QuestionYourFriends/QuestionYourFriendsDataAccess/BusinessManagement/Question.cs
@@ -1,11 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace QuestionYourFriendsDataAccess.BusinessManagement
 {
-    class Question
+    public class Question
     {
         public static bool CreateQuestion(QuestionYourFriendsDataAccess.Question question)
         {
