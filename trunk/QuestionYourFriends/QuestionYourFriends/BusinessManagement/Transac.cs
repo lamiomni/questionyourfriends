45f4669519d2f59670061f881ca41014d6def137
lechevalierd3on@gmail.com <lechevalierd3on@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-05-22 01:42:16 +0000

    Started the Transac system. A few changes to the DB might be needed however.

--- trunk/QuestionYourFriends/QuestionYourFriends/BusinessManagement/Transac.cs
+++ trunk/QuestionYourFriends/QuestionYourFriends/BusinessManagement/Transac.cs
@@ -28,5 +28,38 @@ namespace QuestionYourFriends.BusinessManagement
         {
             return QuestionYourFriendsDataAccess.DataAccess.Transac.GetListTransac(Context.QyfEntities);
         }
+
+        /*
+         *  Higher level methods do deal with the economy 
+         * 
+         */
+        public static bool AnonymiseQuestion(int qid, int bid, int uid)
+        {
+            // a bid can't be lower than a certain value
+            if (bid < (int) QuestionYourFriendsDataAccess.TransacPrice.Anonymize)
+                bid = (int) QuestionYourFriendsDataAccess.TransacPrice.Anonymize;
+
+            // get the user and check his wallet
+            var user = BusinessManagement.User.GetUser(uid);
+            if (user.credit_amount < bid)
+                return false;
+
+            // creation of the transaction
+            var trans = new QuestionYourFriendsDataAccess.Transac
+                            {
+                                amount = bid,
+                                questionId = qid,
+                                userId = uid,
+                                type = (int) QuestionYourFriendsDataAccess.TransacType.Anonymize,
+                                status = (int) QuestionYourFriendsDataAccess.TransacStatus.Ok
+                            };
+            var transCreateRes = CreateTransac(trans);
+           
+            // update of the user's wallet
+            user.credit_amount -= bid;
+            var userModifyRes = BusinessManagement.User.UpdateUser(user);
+
+            return transCreateRes && userModifyRes;
+        }
     }
 }
