ad0d8743d1a2516021f5ff79350b77bc520d3ed2
lechevalierd3on@gmail.com <lechevalierd3on@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-05-26 00:39:14 +0000

    Transactions done

--- trunk/QuestionYourFriends/QuestionYourFriends/BusinessManagement/Transac.cs
+++ trunk/QuestionYourFriends/QuestionYourFriends/BusinessManagement/Transac.cs
@@ -41,25 +41,52 @@ namespace QuestionYourFriends.BusinessManagement
             return QuestionYourFriendsDataAccess.DataAccess.Transac.GetList(Context.QyfEntities);
         }
 
-        /// <summary>
-        /// Higher level methods do deal with the economy
-        /// </summary>
-        /// <param name="question">Question to anonymize</param>
-        /// <param name="bid">Bid value</param>
-        /// <returns>Successfulness of the operation</returns>
-        public static bool AnonymiseQuestion(QuestionYourFriendsDataAccess.Question question, int bid)
+        #region Higher level methods do deal with the economy
+        
+        public static bool AnonymiseQuestion(
+            QuestionYourFriendsDataAccess.Question question,
+            QuestionYourFriendsDataAccess.User user,
+            int bid)
         {
             // A bid can't be lower than a certain value
             if (bid < (int) TransacPrice.Anonymize)
                 bid = (int) TransacPrice.Anonymize;
 
             // Get the user and check his wallet
-            QuestionYourFriendsDataAccess.User user = User.Get(question.id_owner);
             if (user.credit_amount < bid)
                 return false;
 
             // Creation of the transaction
-            bool transCreateRes = Create(bid, question.id_owner, TransacType.Anonymize, question.id);
+            bool transCreateRes = Create(bid, user.id, TransacType.Anonymize, question.id);
+
+            // Update Question's price
+            question.anom_price = bid;
+            
+            // Update of the user's wallet
+            user.credit_amount -= bid;
+            bool userModifyRes = User.Update(user);
+
+            return transCreateRes && userModifyRes;
+        }
+
+        public static bool PrivatizeQuestion(
+            QuestionYourFriendsDataAccess.Question question,
+            QuestionYourFriendsDataAccess.User user,
+            int bid)
+        {
+            // A bid can't be lower than a certain value
+            if (bid < (int)TransacPrice.Privatize)
+                bid = (int)TransacPrice.Privatize;
+
+            // Get the user and check his wallet
+            if (user.credit_amount < bid)
+                return false;
+
+            // Creation of the transaction
+            bool transCreateRes = Create(bid, question.id_owner, TransacType.Privatize, question.id);
+
+            // Update Question's price
+            question.private_price = bid;
 
             // Update of the user's wallet
             user.credit_amount -= bid;
@@ -67,5 +94,101 @@ namespace QuestionYourFriends.BusinessManagement
 
             return transCreateRes && userModifyRes;
         }
+
+        public static bool DesanonymizeQuestion(
+            QuestionYourFriendsDataAccess.Question question,
+            QuestionYourFriendsDataAccess.User user)
+        {
+            var bid = question.anom_price;
+            // A bid can't be lower than a certain value
+            if (bid < (int) TransacPrice.Desanonymize)
+                bid = (int) TransacPrice.Desanonymize;
+
+            // Get the user and check his wallet
+            if (user.credit_amount < bid)
+                return false;
+
+            // Creation of the transaction
+            bool transCreateRes = Create(bid, user.id, TransacType.Desanonymize, question.id);
+
+            // Update Question's price
+            question.anom_price = 0;
+
+            // Update of the user's wallet
+            user.credit_amount -= bid;
+            bool userModifyRes = User.Update(user);
+
+            return transCreateRes && userModifyRes;
+        }
+
+        public static bool DeprivatizeQuestion(
+            QuestionYourFriendsDataAccess.Question question,
+            QuestionYourFriendsDataAccess.User user)
+        {
+            var bid = question.private_price;
+            // A bid can't be lower than a certain value
+            if (bid < (int)TransacPrice.Deprivatize)
+                bid = (int)TransacPrice.Deprivatize;
+
+            // Get the user and check his wallet
+            if (user.credit_amount < bid)
+                return false;
+
+            // Creation of the transaction
+            bool transCreateRes = Create(bid, user.id, TransacType.Deprivatize, question.id);
+
+            // Update Question's price
+            question.private_price = 0;
+
+            // Update of the user's wallet
+            user.credit_amount -= bid;
+            bool userModifyRes = User.Update(user);
+
+            return transCreateRes && userModifyRes;
+        }
+
+        public static bool Purchase(
+            QuestionYourFriendsDataAccess.User user,
+            int amount)
+        {
+            // Creation of the transaction
+            bool transCreateRes = Create(amount, user.id, TransacType.Purchase, 0);
+
+            // Update of the user's wallet
+            user.credit_amount += amount;
+            bool userModifyRes = User.Update(user);
+
+            return transCreateRes && userModifyRes;
+        }
+
+        public static bool EarningStartup(
+            QuestionYourFriendsDataAccess.Question question,
+            QuestionYourFriendsDataAccess.User user)
+        {
+            // Creation of the transaction
+            bool transCreateRes = Create((int)TransacPrice.EarningStartup, user.id, TransacType.EarningStartup, 0);
+
+            // Update of the user's wallet
+            user.credit_amount += (int)TransacPrice.EarningStartup;
+            bool userModifyRes = User.Update(user);
+
+            return transCreateRes && userModifyRes;
+        }
+
+        public static bool EarningAnswer(
+            QuestionYourFriendsDataAccess.User user)
+        {
+            // Creation of the transaction
+            bool transCreateRes = Create((int)TransacPrice.EarningAnswer, user.id, TransacType.EarningAnswer, 0);
+
+            // Update of the user's wallet
+            user.credit_amount += (int)TransacPrice.EarningAnswer;
+            bool userModifyRes = User.Update(user);
+
+            return transCreateRes && userModifyRes;
+        }
+
+
+        #endregion
     }
 }
