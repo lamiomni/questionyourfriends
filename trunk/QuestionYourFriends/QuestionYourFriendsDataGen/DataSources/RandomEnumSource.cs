1019f9c0461a2f9046c5a968e92b620456447898
enixdu94@gmail.com <enixdu94@gmail.com@194d8fa0-555e-5da8-f79f-b48e31366bcf>
2011-06-02 13:47:47 +0000

    comments

--- trunk/QuestionYourFriends/QuestionYourFriendsDataGen/DataSources/RandomEnumSource.cs
+++ trunk/QuestionYourFriends/QuestionYourFriendsDataGen/DataSources/RandomEnumSource.cs
@@ -3,16 +3,28 @@ using AutoPoco.Engine;
 
 namespace QuestionYourFriendsDataGen.DataSources
 {
+    /// <summary>
+    /// Random enum source for AutoPoco
+    /// </summary>
+    /// <typeparam name="T">Enum type</typeparam>
     public class RandomEnumSource<T> : DatasourceBase<int>
     {
         private readonly Array _enums;
         private readonly Random _random = new Random(1337);
 
+        /// <summary>
+        /// Constructor, gets enum values from type
+        /// </summary>
         public RandomEnumSource()
         {
             _enums = Enum.GetValues(typeof (T));
         }
 
+        /// <summary>
+        /// Gets a random value from the previously given enum type
+        /// </summary>
+        /// <param name="context">AutoPoco context</param>
+        /// <returns>Gets a random value from the previously given enum type</returns>
         public override int Next(IGenerationContext context)
         {
             return (int) _enums.GetValue(_random.Next(_enums.Length));
--- trunk/QuestionYourFriends/QuestionYourFriendsDataGen/QyfExtensions.cs
+++ trunk/QuestionYourFriends/QuestionYourFriendsDataGen/QyfExtensions.cs
@@ -4,10 +4,19 @@ using System.Linq;
 
 namespace QuestionYourFriendsDataGen
 {
+    /// <summary>
+    /// Extensions for IEnumerable
+    /// </summary>
     public static class QyfExtensions
     {
         private static readonly Random Rnd = new Random();
 
+        /// <summary>
+        /// Get a value randomly.
+        /// </summary>
+        /// <typeparam name="T">Type of the IEnumerable</typeparam>
+        /// <param name="ienum">IEnumerable container</param>
+        /// <returns>A random item from the container</returns>
         public static T Random<T>(this IEnumerable<T> ienum)
         {
             return ienum.Skip(Rnd.Next(ienum.Count())).FirstOrDefault();
